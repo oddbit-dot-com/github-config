51abf2caf747bcc765ff289bc5fd2dee940b100a
Lars Kellogg-Stedman <lars@oddbit.com>
2026-04-14 13:40:54 -0400

    Migrate baystateradio configuration to new structure
    Migrate the baystateradio organization configuration from the root
    files (organization.go, repositories.go) into the new organization
    package structure using the api types.

---
+++ organizations/baystateradio/organization.go
@@ -0,0 +1,56 @@
+package baystateradio
+
+import (
+	"github.com/oddbit-dot-com/github-config/api"
+	"github.com/pulumi/pulumi-github/sdk/v6/go/github"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+// Organization defines the baystateradio GitHub organization configuration
+var Organization = api.Organization{
+	Name:           "baystateradio",
+	ProviderConfig: api.ProviderFromEnv("GITHUB_TOKEN_BAYSTATERADIO"),
+
+	Settings: &github.OrganizationSettingsArgs{
+		BillingEmail: pulumi.String("lars@oddbit.com"),
+		Blog:         pulumi.String("https://baystateradio.org/news/"),
+		Description:  pulumi.String("Amateur radio information for Eastern Massachusetts and beyond"),
+		Location:     pulumi.String("Boston, MA"),
+	},
+
+	Members: api.Members{
+		"larsks": "admin",
+	},
+
+	DefaultBranchProtection: &github.BranchProtectionArgs{
+		RequiredLinearHistory: pulumi.Bool(true),
+		AllowsForcePushes:     pulumi.Bool(false),
+		EnforceAdmins:         pulumi.Bool(false),
+		ForcePushBypassers:    pulumi.ToStringArray([]string{"/larsks"}),
+		RequiredPullRequestReviews: &github.BranchProtectionRequiredPullRequestReviewArray{
+			github.BranchProtectionRequiredPullRequestReviewArgs{
+				RequiredApprovingReviewCount: pulumi.Int(1),
+			},
+		},
+	},
+
+	Repositories: api.Repositories{
+		"github-config": {
+			RepositoryArgs: &github.RepositoryArgs{
+				Description: pulumi.String("Manage github configuration for baystateradio organization"),
+				AutoInit:    pulumi.Bool(false),
+			},
+		},
+		"baystateradio.org": {
+			RepositoryArgs: &github.RepositoryArgs{
+				Description: pulumi.String("Sources for baystateradio.org website"),
+				HomepageUrl: pulumi.String("https://baystateradio.org"),
+				AutoInit:    pulumi.Bool(false),
+				Pages: &github.RepositoryPagesArgs{
+					BuildType: pulumi.String("workflow"),
+					Cname:     pulumi.String("baystateradio.org"),
+				},
+			},
+		},
+	},
+}
