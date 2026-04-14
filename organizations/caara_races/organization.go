3addd0cc714724c40bf12b5c9ba7c4f05511a101
Lars Kellogg-Stedman <lars@oddbit.com>
2026-04-14 08:26:00 -0400

    Add caara-races and oddbit-dot-com organizations

---
+++ organizations/caara_races/organization.go
@@ -0,0 +1,31 @@
+// organizations/caara_races/organization.go
+package caara_races
+
+import (
+	"github.com/oddbit-dot-com/github-config/api"
+	"github.com/pulumi/pulumi-github/sdk/v6/go/github"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+var Organization = api.Organization{
+	Name:           "caara-races",
+	ProviderConfig: api.ProviderFromEnv("GITHUB_TOKEN_CAARA_RACES"),
+
+	Settings: &github.OrganizationSettingsArgs{
+		BillingEmail: pulumi.String("lars@oddbit.com"),
+	},
+
+	Members: api.Members{
+		"larsks": "admin",
+	},
+
+	Repositories: api.Repositories{
+		"caara-races-website": {
+			RepositoryArgs: &github.RepositoryArgs{
+				Description: pulumi.String("Proposed website for CAARA race and event support"),
+				AutoInit:    pulumi.Bool(false),
+				HomepageUrl: pulumi.String("https://caara-races.oddbit.com/"),
+			},
+		},
+	},
+}
