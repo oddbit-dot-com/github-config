0f73e313c3631bcad3fb8d593711989e3728567c
Lars Kellogg-Stedman <lars@oddbit.com>
2026-04-17 08:45:52 -0400

    Add VaultProviderConfig types and NewVaultProviderConfig/CreateVaultProvider

---
+++ api/vault_provider.go
@@ -0,0 +1,72 @@
+package api
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/oddbit-dot-com/github-config/helpers"
+	vault "github.com/pulumi/pulumi-vault/sdk/v6/go/vault"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type VaultProviderConfig struct {
+	Address    *string
+	Token      *string
+	MountPoint string
+}
+
+type VaultSecretRef struct {
+	Path string
+	Key  string
+}
+
+type OrgSecretRef struct {
+	VaultSecretRef
+	Visibility string
+}
+
+type ActionsSecrets map[string]VaultSecretRef
+type OrgActionsSecrets map[string]OrgSecretRef
+
+func NewVaultProviderConfig(address, token string, mountpoint ...string) *VaultProviderConfig {
+	if address == "" {
+		address = os.Getenv("VAULT_ADDR")
+	}
+	if token == "" {
+		token = os.Getenv("VAULT_TOKEN")
+	}
+	mount := "secret"
+	if len(mountpoint) > 0 && mountpoint[0] != "" {
+		mount = mountpoint[0]
+	}
+	cfg := &VaultProviderConfig{MountPoint: strings.TrimPrefix(strings.TrimSuffix(mount, "/"), "/")}
+	if address != "" {
+		cfg.Address = &address
+	}
+	if token != "" {
+		cfg.Token = &token
+	}
+	return cfg
+}
+
+func CreateVaultProvider(ctx *pulumi.Context, config *VaultProviderConfig, orgName string) (*vault.Provider, error) {
+	if config == nil {
+		return nil, nil
+	}
+	if config.Address == nil {
+		return nil, fmt.Errorf("vault address not configured for %s: set VAULT_ADDR or pass an explicit address", orgName)
+	}
+	if config.Token == nil {
+		return nil, fmt.Errorf("vault token not configured for %s: set VAULT_TOKEN or pass an explicit token", orgName)
+	}
+
+	providerArgs := &vault.ProviderArgs{
+		Address:       pulumi.String(*config.Address),
+		Token:         pulumi.String(*config.Token),
+		SkipTlsVerify: pulumi.Bool(false),
+	}
+
+	resourceName := fmt.Sprintf("vault-provider.%s", helpers.Slugify(orgName))
+	return vault.NewProvider(ctx, resourceName, providerArgs)
+}
