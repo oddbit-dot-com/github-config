0d01579ee03439b751ce569e261b3ab547f20e6b
Lars Kellogg-Stedman <lars@oddbit.com>
2026-04-24 09:36:03 -0400

    Add manymanymeatballs organization

---
+++ organizations/manymanymeatballs/organization.go
@@ -0,0 +1,44 @@
+package manymanymeatballs
+
+import (
+	"github.com/oddbit-dot-com/github-config/api"
+	"github.com/pulumi/pulumi-github/sdk/v6/go/github"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+// Organization defines the baystateradio GitHub organization configuration
+var Organization = api.Organization{
+	Name:                 "manymanymeatballs",
+	GithubProviderConfig: api.NewGithubProviderConfig(),
+
+	Settings: &github.OrganizationSettingsArgs{
+		BillingEmail: pulumi.String("lars@oddbit.com"),
+		Description:  pulumi.String("Mmm, meatballs."),
+		Location:     pulumi.String("Boston, MA"),
+	},
+
+	Members: api.Members{
+		"larsks": api.PermissionAdmin,
+	},
+
+	Repositories: []*api.Repository{
+		{
+			Name: "manymanymeatballs.com",
+			RepositoryArgs: &github.RepositoryArgs{
+				Description:   pulumi.String("Sources for manymanymeatballs.com website"),
+				DefaultBranch: pulumi.String("gh-pages"),
+				HomepageUrl:   pulumi.String("https://manymanymeatballs.com"),
+				AutoInit:      pulumi.Bool(false),
+				Pages: &github.RepositoryPagesArgs{
+					BuildType: pulumi.String(api.PageBuildLegacy),
+					Cname:     pulumi.String("manymanymeatballs.com"),
+					Source: &github.RepositoryPagesSourceArgs{
+						Branch: pulumi.String("gh-pages"),
+						Path:   pulumi.String("/"),
+					},
+				},
+			},
+			BranchProtectionRules: api.BranchProtectionRules{},
+		},
+	},
+}
